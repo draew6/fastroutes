08a2e3d6db2b9c1c15197a1b0d0f552541b18aa3
draew6 <adosko6@gmail.com>
2025-06-18 13:39:16 +0200

    handle_errors added error info

--- fastroutes/api.py
+++ fastroutes/api.py
@@ -3,12 +3,13 @@ from fastapi import HTTPException
 from httpx import HTTPStatusError
 
 
-async def handle_errors[T](call: Awaitable[T]) -> T:
+async def handle_errors[T](call: Awaitable[T], info: str | None = None) -> T:
     result = await call
     if isinstance(result, HTTPStatusError):
         raise HTTPException(
             status_code=result.response.status_code,
             detail=result.response.text,
+            headers={"X-Error-Info": info} if info else None,
         )
     elif isinstance(result, BaseException):
         raise result
