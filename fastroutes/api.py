ed20343d13d1f81beb744078d9602d08a4528cb6
draew6 <adosko6@gmail.com>
2025-07-20 18:16:19 +0200

    revert

--- fastroutes/api.py
+++ fastroutes/api.py
@@ -4,13 +4,13 @@ from httpx import HTTPStatusError
 
 
 async def handle_errors[T](call: Awaitable[T], info: str | None = None) -> T:
-    try:
-        return await call
-    except HTTPStatusError as e:
+    result = await call
+    if isinstance(result, HTTPStatusError):
         raise HTTPException(
-            status_code=e.response.status_code,
-            detail=e.response.text,
+            status_code=result.response.status_code,
+            detail=result.response.text,
             headers={"X-Error-Info": info} if info else None,
         )
-    except Exception as e:
-        raise e
+    elif isinstance(result, BaseException):
+        raise result
+    return result
